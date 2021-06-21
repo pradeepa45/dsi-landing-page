d9d44b62bff6aa34f25ca6215be749bda5170fe2
Pradeepa Seelam <39674626+pradeepa45@users.noreply.github.com>
2021-06-21 14:46:58 +0530

    para adjustment

--- src/Welcome.js
+++ src/Welcome.js
@@ -115,11 +115,12 @@ export default function Welcome() {
                         <Typography className={classes.heading}>
                             Welcome to <span className={classes.spanClass}> Dwidasa Samsara Indonesia (DSI)</span>
                         </Typography>
+                        <br />
                         <Typography variant='body1' className={classes.text}>
-                            Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.
+                            Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through <span className={classes.spanClass}>DSI</span>’s distinct front-end based application concept.
                         </Typography>
                         <Typography variant='body1' className={classes.text}>
-                            Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.
+                            Managed by a team of professional experts with extensive experience and impressive track records, <span className={classes.spanClass}>DSI</span> believes that continuous improvements and innovations assure your business to run effectively and efficiently.
                         </Typography>
 
                     </Grid>
